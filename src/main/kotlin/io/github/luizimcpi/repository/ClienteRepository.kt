f4bdb58c24ff7feac4fed9779c62b43ff6a01a74
Luiz Henrique <luizhenrique.se@gmail.com>
2024-03-04 13:00:21 -0300

    fix: add forupdate findclientebyid

--- src/main/kotlin/io/github/luizimcpi/repository/ClienteRepository.kt
+++ src/main/kotlin/io/github/luizimcpi/repository/ClienteRepository.kt
@@ -1,10 +1,15 @@
 package io.github.luizimcpi.repository
 
 import io.github.luizimcpi.model.entity.Cliente
+import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 
 @Repository
 interface ClienteRepository: JpaRepository<Cliente, Long> {
+
+    @Query("select * from clientes WHERE id = :clienteId FOR UPDATE", nativeQuery = true)
+    fun findByClienteByIdForUpdate(clienteId: Long): Optional<Cliente>
 }
